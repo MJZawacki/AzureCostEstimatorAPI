888b3ebfb35c796cb713238f1acd17b64d621485
Michael Zawacki <mzawacki@microsoft.com>
2019-04-24 15:34:05 -0700

    increase test time for blob tests

--- src/RateTableBlobStore.spec.ts
+++ src/RateTableBlobStore.spec.ts
@@ -4,12 +4,15 @@ import { RateTable, Sku, Meter, CostInput } from './RateTable';
 import { RateTableBlobStore } from './RateTableBlobStore';
 import { FunctionUtil } from './FunctionUtil';
 import * as fs from 'fs';
-import { AssertionError } from 'assert';
+import { AssertionError, doesNotReject } from 'assert';
+import { exponentialRetryPolicy } from '@azure/ms-rest-js';
+import * as config from "config";
 
 describe('RateTableBlobStore', () => {
 
-  it('saveRateTable should save file to BlobStore', async () => {
 
+  it('saveRateTable should save file to BlobStore', async function() {
+    this.timeout(20000);
     let store = new RateTableBlobStore();
 
     let testobj = {
@@ -19,10 +22,11 @@ describe('RateTableBlobStore', () => {
     let url = await store.saveRateTable('test', new RateTable(testobj));
     expect(url).to.not.be.null;
     // assert.doesNotThrow(await store.saveRateTable('test', <RateTable>testobj));
+
   });
  
-  it('getRateTable should return rates lookuptable', async () => {
-
+  it('getRateTable should return rates lookuptable', async function() {
+    this.timeout(20000);
     let store = new RateTableBlobStore();
 
     let rates = await store.getRateTable('test');
@@ -30,7 +34,7 @@ describe('RateTableBlobStore', () => {
     expect(rates).to.be.a('object');
     expect(rates).to.have.property('_skus');
     expect(rates).to.have.property('_datacenters');
-  
+   
   });
 
 });
